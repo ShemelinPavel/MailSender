2ed02d575a758d22c4911857fe703a624b74780a
ShemelinPavel <pauls0@mail.ru>
2019-05-17 19:44:21 +0300

    внедрение mvvm

---
+++ MailSender/ViewModel/WpfMainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using WpfMailSender.lib.Data.LinqToSQL;
+using WpfMailSender.lib.Services.Interfaces;
+
+namespace WpfMailSender.ViewModel
+{
+    public class WpfMainWindowViewModel : ViewModelBase
+    {
+        private string _title = "Рассылка писем wpf+mvvm";
+        private readonly IRecipientsData recipData;
+        private ObservableCollection<Recipient> recipCol;
+
+
+        public string mvvmTitle
+        {
+            get => _title;
+            set => Set ( ref _title, value );
+        }
+
+        public ObservableCollection<Recipient> mvvmRecipients
+        {
+            get => recipCol;
+            set => Set ( ref recipCol, value );
+        }
+
+        public WpfMainWindowViewModel ( IRecipientsData recipientsData )
+        {
+            recipData = recipientsData;
+
+            LoadData ();
+        }
+
+        private void LoadData ()
+        {
+            recipCol = new ObservableCollection<Recipient> ( recipData.GetAll () );
+        }
+    }
+}
---
+++ WpfMailSender.lib/Services/RecipientsDataLinqToSQL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMailSender.lib.Data.LinqToSQL;
+using WpfMailSender.lib.Services.Interfaces;
+
+namespace WpfMailSender.lib.Services
+{
+    public class RecipientsDataLinqToSQL : IRecipientsData
+    {
+        private readonly MailSenderDB dbcontext;
+
+        public RecipientsDataLinqToSQL (MailSenderDB databaseContext)
+        {
+            dbcontext = databaseContext;
+        }
+
+        public IEnumerable<Recipient> GetAll ()
+        {
+            return dbcontext.Recipient.ToArray ();
+        }
+    }
+}
